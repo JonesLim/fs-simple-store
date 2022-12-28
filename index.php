589660bd6279ce83f7ee021a5bef4aa26016f8aa
JonesLim <tzercheng22l@forwardschool.edu.my>
2022-12-28 09:43:39 +0800

    update

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <div class="d-flex justify-content-between align-items-center mb-4">
           <h1 class="h1">My Store</h1>
           <div class="d-flex align-items-center justify-content-end gap-3">
-            <a href="cart.php" class="btn btn-success">My Cart</a>
+            <a href="/cart" class="btn btn-success">My Cart</a>
           </div>
         </div>
 
@@ -109,15 +109,15 @@
       <!-- footer -->
       <div class="d-flex justify-content-between align-items-center pt-4 pb-2">
         <div class="text-muted small">
-          © 2022 <a href="index.php" class="text-muted">My Store</a>
+          © 2022 <a href="/" class="text-muted">My Store</a>
         </div>
         <div class="d-flex align-items-center gap-3">
         <?php if ( isLoggedIn() ) : ?>
-          <a href="orders.php" class="btn btn-light btn-sm">My Orders</a>
-          <a href="logout.php" class="btn btn-light btn-sm">Logout</a>
+          <a href="/orders" class="btn btn-light btn-sm">My Orders</a>
+          <a href="/logout" class="btn btn-light btn-sm">Logout</a>
         <?php else : ?>
-          <a href="login.php" class="btn btn-light btn-sm">Login</a>
-          <a href="signup.php" class="btn btn-light btn-sm">Sign Up</a>
+          <a href="/login" class="btn btn-light btn-sm">Login</a>
+          <a href="/signup" class="btn btn-light btn-sm">Sign Up</a>
         <?php endif; ?>
         </div>
       </div>
